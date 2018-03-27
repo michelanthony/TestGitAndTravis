f37a5c00d1fcb885a5f743b6d01f20eb3bfad526
michelanthony <anthony.michel@free.fr>
2018-03-27 22:09:02 +0000

    Create hello.cpp

---
+++ hello.cpp
@@ -0,0 +1,10 @@
+#include <iostream>    
+#include <cmath>
+
+int main()            
+{   
+    std::cout << "Hello, World! C++ " << std::endl;
+    double x = 5; 
+    double y = std::cos(x);
+    std::cout << y << std::endl;
+}
